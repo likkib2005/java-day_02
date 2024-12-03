3f439e72cc0937d5ce06aded8fb538532ee3ae5f
likkib2005 <likkilikitha214@gmail.com>
2024-12-03 13:38:02 +0530

    Add files via upload

---
+++ bitwiseor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class bitwiseor
+{
+    public static void main(String args[])
+    {
+        System.out.println(13|5);
+        System.out.println(10|12);
+        System.out.println(11|18);
+        System.out.println(22|144);
+        System.out.println(88|122);
+        System.out.println(12|11);
+        System.out.println(13|18);
+        
+    }
+}
