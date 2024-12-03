259e6f95537d000c4f7de805e08e8ffc491a8c2f
likkib2005 <likkilikitha214@gmail.com>
2024-12-03 10:55:37 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class assignment
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=14;
+        System.out.println(a+=46);
+        System.out.println(a-=89);
+        System.out.println(a*=12);
+        System.out.println(a/=5);
+        System.out.println(a%=6);
+
+    }
+}
