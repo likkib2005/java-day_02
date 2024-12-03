ce7dbb447bbd9725170e6e4ba1e762e9f84a9422
likkib2005 <likkilikitha214@gmail.com>
2024-12-03 13:48:55 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class logical
+{
+public static void main(String args[])
+{
+System.out.println(true&&true);
+System.out.println(true||false);
+System.out.println(!(true));   
+}
+}
