6681251373afd6f7eeb308f0beb7f5183abb61d6
likkib2005 <likkilikitha214@gmail.com>
2024-12-03 10:09:36 +0530

    Add files via upload

---
+++ arithmetic_operators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class arithmetic_operators
+{
+    public static void main(String args[])
+    {
+    int a=12;
+    int b=14;
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a%b;
+    int g=a/b;
+    System.out.println("addtion="+c);
+    System.out.println("subtraction="+d);
+    System.out.println("multiplication="+e);
+    System.out.println("remainder_division="+f);
+    System.out.println("Quotient="+g);
+    }
+}
