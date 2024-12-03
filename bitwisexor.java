be2421a603867cabd410c08488d419383c299016
likkib2005 <likkilikitha214@gmail.com>
2024-12-03 13:56:43 +0530

    Add files via upload

---
+++ bitwisexor.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class bitwisexor
+{
+    public static void main(String args[])
+    {
+    System.out.println(122^122);
+    System.out.println(130^140);
+    System.out.println(120^130);
+    System.out.println(110^110);
+    System.out.println(5^5);
+    }
+}
